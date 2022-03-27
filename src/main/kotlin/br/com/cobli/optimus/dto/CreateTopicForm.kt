a4d64b7608380808bf3b12a1440ad50e0b33df37
matheus-reyes <matheuscreyes@gmail.com>
2022-03-27 01:05:57 -0300

    feat: adding serverError and validationError handlers

--- src/main/kotlin/br/com/cobli/optimus/dto/CreateTopicForm.kt
+++ src/main/kotlin/br/com/cobli/optimus/dto/CreateTopicForm.kt
@@ -6,8 +6,13 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class CreateTopicForm(
-    @field:NotEmpty @field:Size(min = 5, max = 100) val title: String,
-    @field:NotEmpty val message: String,
-    @field:NotNull val courseId: UUID,
-    @field:NotNull val authorId: UUID,
+    @field:NotEmpty(message = "title cannot be empty")
+    @field:Size(min = 5, max = 100, message = "title must be between 5 and 100 characters")
+    val title: String,
+    @field:NotEmpty(message = "message cannot be empty")
+    val message: String,
+    @field:NotNull(message = "courseId cannot be empty")
+    val courseId: UUID,
+    @field:NotNull(message = "authorId cannot be empty")
+    val authorId: UUID,
 )
--- src/main/kotlin/br/com/cobli/optimus/dto/UpdateTopicForm.kt
+++ src/main/kotlin/br/com/cobli/optimus/dto/UpdateTopicForm.kt
@@ -6,7 +6,11 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class UpdateTopicForm(
-    @field:NotNull val id: UUID,
-    @field:NotEmpty @field:Size(min = 5, max = 100) val title: String,
-    @field:NotEmpty val message: String,
+    @field:NotNull(message = "id cannot be empty")
+    val id: UUID,
+    @field:NotEmpty(message = "title cannot be empty")
+    @field:Size(min = 5, max = 100, message = "title must be between 5 and 100 characters")
+    val title: String,
+    @field:NotEmpty(message = "message cannot be empty")
+    val message: String,
 )
